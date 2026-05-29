836153ae03184115cbcf9ce46a5566498465c23b
sanchescorreia822-stack <sanchescorreia822@gmail.com>
2026-05-29 21:35:27 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const TelegramBot = require("node-telegram-bot-api");
 
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
-const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: false });
-const chatId = process.env.CHAT_ID;
+const bot = new TelegramBot(process.env.BOT_TOKEN || "", { polling: false });
+const chatId = process.env.CHAT_ID || "";
 
 app.get("/", (req, res) => {
   res.send("Football Studio Bot Online ✅");
@@ -22,5 +22,6 @@ app.post("/signal", (req, res) => {
     message: "Signal recebido"
   });
 });
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor online na porta ${PORT}`));
